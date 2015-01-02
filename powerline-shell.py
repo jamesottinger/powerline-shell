d016e21edce016077fbd50e11e65c991fa8a4ac0
James Ottinger <ottinger.james@gmail.com>
2015-01-02 12:00:09 -0500

    me like this green better

--- powerline-shell.py
+++ powerline-shell.py
@@ -205,7 +205,7 @@ class DefaultColor:
     SSH_BG = 166 # medium orange
     SSH_FG = 254
 
-    REPO_CLEAN_BG = 148  # a light green color
+    REPO_CLEAN_BG = 41  # a light green color
     REPO_CLEAN_FG = 0  # black
     REPO_DIRTY_BG = 161  # pink/red
     REPO_DIRTY_FG = 15  # white
